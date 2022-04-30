417f3846a899f3328b28dab7e4c8059fceae5b05
Nam Anh Mai <maithenamanh@gmail.com>
2022-04-29 23:43:41 -0700

    Added function to create squares in grid container

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-/* ----------------------------------------------------------------
-Create grid of square divs
+/*  ----------------------------------------------------------------
+    Create grid of square divs
 */
 function createGrid() {
     const containerDiv = document.getElementById('container');
-    const squaresPerSide = 5;
+    const squaresPerSide = 10;
+    for (let i = 1; i < squaresPerSide ** 2; i++) {
+        createSquare();
+    }
     const gridAttributes = {
         style: `grid-template-columns: repeat(${squaresPerSide}, 1fr); grid-template-rows: repeat(${squaresPerSide}, 1fr);`
     }
     setMultipleAttributes(containerDiv, gridAttributes);
-
-    // document.body.appendChild(containerDiv);
 }
 
 function setMultipleAttributes(element, attributes) {
@@ -18,9 +19,14 @@ function setMultipleAttributes(element, attributes) {
     });
 }
 
+/*  -------------------------------------------------------------------
+    Create square grid item inside grid container
+*/
 function createSquare() {
-    const containerDiv = document.getElementsByClassName('container');
+    const containerDiv = document.getElementById('container');
     const square = document.createElement('div');
+    square.classList.add('grid-item');
+    square.setAttribute('style', 'border: 0.1rem solid blue;');
     containerDiv.appendChild(square);
 }
 
